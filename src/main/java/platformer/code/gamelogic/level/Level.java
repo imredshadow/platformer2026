cec1bb6678fd70957fbf20e2ea549875748f97c7
imredshadow <jianturalbaacol@gmail.com>
2026-05-20 18:28:06 +0000

    67

--- src/main/java/platformer/code/gamelogic/level/Level.java
+++ src/main/java/platformer/code/gamelogic/level/Level.java
@@ -197,7 +197,21 @@ public class Level {
 	//Your code goes here! 
 	//Please make sure you read the rubric/directions carefully and implement the solution recursively!
 	private void water(int col, int row, Map map, int fullness) {
-		
+		Water w = new Water(col, row, tileSize, tileset.getImage("Full_water"), this, fullness);
+		map.addTile(col, row, w);
+		//check if we can go down
+		if(row+1 < map.getTiles()[0].length && !(map.getTiles()[col][row+1] instanceof Water) && !(map.getTiles()[col][row+1].isSolid())) {
+			water(col, row+1, map, 3);
+		}
+        //if we can’t go down go left and right.
+		//right
+		if(col+1 < map.getTiles().length && !(map.getTiles()[col+1][row] instanceof Water) && !(map.getTiles()[col+1][row].isSolid())) {
+			water(col+1, row, map, 3);
+		}
+		//left
+		if(col-1 >= 0 && !(map.getTiles()[col-1][row] instanceof Water) && !(map.getTiles()[col-1][row].isSolid())) {
+			water(col-1, row, map, 3);
+		}
 	}
 
 
